e2320cede1d9caf8ab8425ae6fbc7e8779fe2adf
Joseph Groocock <frebib@gmail.com>
2015-12-06 02:14:13 +0000

    Add more checks on logout

--- src/net/frebib/servlet/LogoutServlet.java
+++ src/net/frebib/servlet/LogoutServlet.java
@@ -18,13 +18,22 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            SessionManager.getFromSession(req.getSession()).dispose(req.getSession());
             // Send a pretty popup message
             PopupMessage.set("Logged Out", "You logged out successfully.<br/>Please log back in to send more emails",
                     "login", req.getSession());
+
+            // Clear the logged-in mail session
+            SessionManager sessionMgr = SessionManager.getFromSession(req.getSession());
+            if (sessionMgr != null)
+                sessionMgr.dispose(req.getSession());
+
+            // Clear the web session
+            //## !This will prevent the pretty message from showing, so don't!
+            //req.getSession().invalidate();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            // Redirect back to the login page
             resp.sendRedirect("/login");
         }
     }
